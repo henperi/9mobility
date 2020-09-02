cfcb668fab8e6b98d0a190d7d443bfeee1cb7839
henry <henperi.io@gmail.com>
2020-09-02 12:30:27 +0100

    bootstrap the react app - add styled components - add eslint, prettier and some nice linter configs - add styleliner, and some dev plugins for styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+/**
+ *  React Function
+ *  @returns Element
+ */
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
