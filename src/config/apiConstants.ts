56b2310fdb1d3d96fe9bb397cd489c71369d3b6e
Henry Izontimi <pienry3g@gmail.com>
2020-10-27 16:36:11 +0100

    up date to dev environment

--- src/config/apiConstants.ts
+++ src/config/apiConstants.ts
@@ -1,4 +1,4 @@
 export const apiConstants = {
-  // apiUrl: 'http://40.76.69.211/',
-  apiUrl: 'https://prod-api.9mobile.com.ng/',
+  apiUrl: 'http://40.76.69.211/',
+  // apiUrl: 'https://prod-api.9mobile.com.ng/',
 };
